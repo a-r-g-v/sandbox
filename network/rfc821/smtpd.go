2d47fb128bbc92fe2e0f948217e48eb5dcd43b82
greyia <info@thun.ws>
2015-07-02 08:25:38 +0900

    Add Logging

--- network/rfc821/smtpd.go
+++ network/rfc821/smtpd.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"net"
+	"strings"
 )
 
 func main() {
+	log.SetLevel(log.DebugLevel)
 	service := ":25"
 	tcpAddr, _ := net.ResolveTCPAddr("tcp", service)
 	listner, _ := net.ListenTCP("tcp", tcpAddr)
@@ -23,24 +26,37 @@ func main() {
 func session(conn net.Conn) {
 	defer conn.Close()
 	var senderDomain string
+	var body string
+	addr := conn.RemoteAddr().String()
 
 	conn.Write([]byte("220 takoyaki SMTP Service Ready\r\n"))
+	log.WithFields(log.Fields{"level": "info", "state": "ACCEPT", "addr": addr}).Info("Accept")
 
 	stage := 0
 	messageBuf := make([]byte, 512)
 
 	for {
 		messageLen, _ := conn.Read(messageBuf)
+		log.WithFields(log.Fields{"level": "debug", "addr": addr, "domain": senderDomain, "stage": stage, "messageLen": messageLen}).Debug("Read")
 		if messageLen <= 0 {
 			return
 		}
+
 		message := string(messageBuf[:messageLen])
+
+		if messageLen <= 4 {
+			log.WithFields(log.Fields{"level": "info", "state": "unrecognized", "addr": addr, "domain": senderDomain, "stage": stage}).Info("unrecognized: " + strings.Trim(message, "\n"))
+			conn.Write([]byte("500 5.5.1 Command unrecognized\r\n"))
+			continue
+		}
+
 		command := string(message[:4])
-		body := string(message[5:messageLen])
+		body = strings.Trim(string(message[5:messageLen]), "\n")
+		log.WithFields(log.Fields{"level": "debug", "addr": addr, "domain": senderDomain, "stage": stage, "messageLen": messageLen, "command": command, "body": body}).Debug("Generate")
 
 		if command == "HELO" && stage == 0 {
 			senderDomain = body
-			fmt.Printf("Accept HELO Domain:%s \n", senderDomain)
+			log.WithFields(log.Fields{"level": "info", "state": "ACCEPT", "addr": addr, "domain": senderDomain, "stage": stage}).Info("Accept HELO")
 			stage = 1
 			conn.Write([]byte("250 pleased to meet you\r\n"))
 		} else if command == "MAIL" && stage == 1 {
@@ -54,11 +70,13 @@ func session(conn net.Conn) {
 			conn.Write([]byte("354 Start mail input; end with <CRLF>.<CRLF>\r\n"))
 			conn.Write([]byte("250 OK\r\n"))
 		} else if command == "QUIT" {
+			log.WithFields(log.Fields{"level": "info", "state": "QUIT", "addr": addr, "domain": senderDomain, "stage": stage}).Info("Accept QUIT")
 			conn.Write([]byte("221 Service closing transmission channel\r\n"))
 			return
 		} else if command == "NOOP" {
 			conn.Write([]byte("250 OK\r\n"))
 		} else if command == "RSET" && stage >= 1 {
+			log.WithFields(log.Fields{"level": "info", "state": "reset", "addr": addr, "domain": senderDomain, "stage": stage}).Info("accept rset")
 			stage = 1
 			conn.Write([]byte("250 OK\r\n"))
 		} else if command == "VRFY" && stage == 1 {
@@ -71,6 +89,7 @@ func session(conn net.Conn) {
 			conn.Write([]byte("214-Command supported\r\n"))
 			conn.Write([]byte("214 HELO MAIL RCPT DATA NOOP QUIT REST HELP\r\n"))
 		} else {
+			log.WithFields(log.Fields{"level": "info", "state": "unrecognized", "addr": addr, "domain": senderDomain, "stage": stage}).Info("unrecognized: " + strings.Trim(message, "\n"))
 			conn.Write([]byte("500 5.5.1 Command unrecognized\r\n"))
 		}
 	}
